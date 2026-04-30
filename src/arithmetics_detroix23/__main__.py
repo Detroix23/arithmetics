127f5da796707372723367cdf19550274157454a
Detroix23 <detroix23@gmail.com>
2026-04-30 22:06:43 +0200

    Built a rudimentary CLI.

--- src/arithmetics_detroix23/__main__.py
+++ src/arithmetics_detroix23/__main__.py
@@ -3,34 +3,14 @@
 /src/arithmetics_detroix23/__main__.py
 """
 
-from arithmetics_detroix23 import (
-	digital_counters, 
-	consecutive_signed_sums,
-	predators_prays,
-	dividers,
-	conway_sequence,
-)
+import sys
+
+from arithmetics_detroix23.shared import cli
 
 def main() -> None:
 	"""
 	Main entry point to test proposed arithmetic problems.
 	"""
-	
-	# Digital counters.
-	# digital_counters.tests.main()
-
-	# Consecutive signed sums.
-	# consecutive_signed_sums.forms.main()
-
-	# Prays and predators.
-	# predators_prays.tests.capes1()
-
-	# Dividers, GCP, primes.
-	#dividers.tests.euclid_result1()
-	#dividers.tests.gcd_scatter()
-	#dividers.tests.gcd_study1()
-
-	# Conway sequence.
-	conway_sequence.tests.compress1()
+	cli.run_with_arguments(sys.argv)
 
 main()
---
+++ src/arithmetics_detroix23/shared/cli.py
@@ -0,0 +1,58 @@
+"""
+# Arithmetics: shared.
+/src/shared/cli.py
+"""
+
+from arithmetics_detroix23 import (
+	digital_counters, 
+	consecutive_signed_sums,
+	predators_prays,
+	dividers,
+	conway_sequence,
+)
+
+HELP_MESSAGE: str = """
+## Help.
+```bash
+py __main__.py <script>
+```
+
+**Arguments**:
+`script`:
+- `dg`;
+- `css`;
+- `pp`;
+- `gcd`;
+- `cs`;
+"""
+
+def run_with_arguments(arguments: list[str]) -> None:	
+	"""
+	Execute the request script.
+	"""
+	print("# Arithmetics.")
+
+	if len(arguments) <= 1:
+		print("Missing <script> at position 1.")
+		print(HELP_MESSAGE)
+
+	elif arguments[1] in {"dg", "counters"}:
+		digital_counters.tests.main()
+
+	elif arguments[1] in {"css", "sums"}:
+		consecutive_signed_sums.forms.main()
+
+	elif arguments[1] in {"pp", "predators", "preys"}:
+		predators_prays.tests.capes1()
+
+	elif arguments[1] in {"gcd", "dividers"}:
+		dividers.tests.euclid_result1()
+		dividers.tests.gcd_scatter()
+		dividers.tests.gcd_study1()
+
+	elif arguments[1] in {"cs", "conway"}:
+		conway_sequence.tests.compress1()
+
+	else:
+		print("Incorrect <script> at position 1.")
+		print(HELP_MESSAGE)
